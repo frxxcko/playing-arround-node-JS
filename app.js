9e15a2598d854b3aaee2c35650cd07bd21688423
Juan Basualdo <juan.i.basualdo@accenture.com>
2022-07-23 15:35:53 -0300

    In package.json I've modified the type property to 'module' so I can work with import statements and no with Common JS

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const validator = require('validator')
+import chalk from 'chalk';
+import validator from 'validator';
 
+const log = console.log;
+
+const msg = 'Hola mundo'
+
+log(validator.isEmail('pepito@gmail.com'))
+log(chalk.green(msg))
