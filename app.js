10bc6debcf55e2b59d5d9324962071a9c4e62cb3
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-24 15:26:42 -0300

    Added validations for 'add' command to end the execution if --title or --body are empty

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const yargs = require('yargs')
 const { argv } = require('yargs')
-const notes = require('./notes')
-
+const validator = require('validator')
+const notes = require('./notes');
+const { default: chalk } = require('chalk');
+const log = console.log;
 
 
 yargs.command({
     command: 'add',
     describe: 'Add a note',
     handler: function() {
+        const titleIsEmpty = validator.isEmpty(argv.title);
+        const bodyIsEmpty = validator.isEmpty(argv.body);
+        if(titleIsEmpty || bodyIsEmpty)
+        {
+            log(chalk.red(`Please provide a valid ${titleIsEmpty? 'title' : bodyIsEmpty? 'body' : ""}`));
+            return;
+        }
         notes.addNote(argv.title, argv.body);
     },
     builder: {
@@ -36,6 +45,11 @@ yargs.command({
     command: 'remove',
     describe: 'Remove a note by Title',
     handler: function() {
+        if(validator.isEmpty(argv.title))
+        {
+            log(chalk.red('Please provide a valid Title to remove'));
+            return;
+        }
         notes.removeNote(argv.title)
     },
     builder: {
@@ -50,7 +64,12 @@ yargs.command({
 yargs.command({
     command: "get",
     describe: "Get Note by it's title",
-    handler: function(title) {
+    handler: function() {
+        if(validator.isEmpty(argv.title))
+        {
+            log(chalk.red('Please provide a valid Title'));
+            return;
+        }
         notes.getNoteByTitle(argv.title);
     },
     builder: {
