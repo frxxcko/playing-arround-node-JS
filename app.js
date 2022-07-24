306b1f8c225084b9a7fea659427a1d6aebe8dc00
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-23 23:28:06 -0300

    Playing with yargs

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 
+const log = console.log;
+
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists all notes',
+    handler:  () => log('Listing all notes...')
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads all notes',
+    handler:  () => log('Reading all notes...')
+})
+
+log(yargs.argv)
