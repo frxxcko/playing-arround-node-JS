7a5aaf65d42602c5b988d38c3ae317d1d35d1ebd
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-24 15:54:28 -0300

    Refactor addNote method added a getAll method to retrieve all notes or an empty array to push notes into

--- notes.js
+++ notes.js
@@ -1,59 +1,53 @@
-const fs = require('fs')
-const validator = require('validator')
-const chalk = require('chalk')
-const notesPath = 'notes.json'
+const fs = require('fs');
+const validator = require('validator');
+const chalk = require('chalk');
+const notesPath = 'notes.json';
 const log = console.log;
-const notesJSON = fs.readFileSync(notesPath, { encoding: "utf-8" })
 
-const getNoteByTitle = (title) => console.table(Array(JSON.parse(notesJSON).find( note => note.title = title)));
-
-const addNote = (title, body) => {
-    const notes = notesJSON || [];
-    
-    const NOTE = {
-        title: title,
-        body: body
-    };
-
-    if(typeof notes === "object" && notes.length === 0)
-    {
-        notes.push(NOTE);
-        fs.writeFileSync(notesPath, JSON.stringify(notes));
-        log(chalk.green.inverse('First note added'));
-        return;
+const getAll = () => {
+    try {
+        const notesJSON = fs.readFileSync(notesPath, { encoding: "utf-8" });
+        return JSON.parse(notesJSON);
     }
-    else if(typeof notes === 'string' && notes)
-    {
-        let notesParsed = JSON.parse(notes);
-        if(!notesParsed.find( note => note.title === NOTE.title))
-        {
-            notesParsed.push(NOTE);
-            fs.writeFileSync(notesPath, JSON.stringify(notesParsed));
-            log(chalk.green.inverse('Note added to the list'));
-            return;
-        }
-        log(chalk.red("Title already added"))
+    catch (error) {
+        return [];
     }
 }
 
+const addNote = (title, body) => {
+    const notes = getAll();
+    
+    if (notes.find( note => note.title === title.trim())) {
+        log(chalk.red("Note already added"))
+        return;
+    }
+
+    const NOTE = {
+        title: title.trim(),
+        body: body.trim()
+    };
+
+    notes.push(NOTE);
+    fs.writeFileSync(notesPath, JSON.stringify(notes));
+    log(chalk.green('Note added to the list'));
+}
+
+const getNoteByTitle = (title) => console.table(Array(JSON.parse(notesJSON).find(note => note.title = title)));
+
 const listAll = () => {
-    if(notesJSON) console.table(JSON.parse(notesJSON));
+    if (notesJSON) console.table(JSON.parse(notesJSON));
 }
 
 const removeNote = (title) => {
-    if(notesJSON)
-    {
-        const notesParsed = JSON.parse(notesJSON);
-        if(notesParsed.find( note => note.title === title))
-        {
-            const filteredNotes = notesParsed.filter( note => note.title !== title);
-            fs.writeFileSync(notesPath, JSON.stringify(filteredNotes));
-            log(chalk.red.inverse(`Note titled "${title}" has been removed`));
-            return;
-        }
+    const notes = getAll();
+    if (notes.find(note => note.title === title)) {
+        const filteredNotes = notes.filter(note => note.title !== title);
+        fs.writeFileSync(notesPath, JSON.stringify(filteredNotes));
+        log(chalk.red(`Note titled "${title}" has been removed`));
+        return;
     }
 
-    log(chalk.red("Please check if provided Title exists or it is well written"))
+    log(chalk.red("Please check if provided Title is well written"))
 }
 
 module.exports = { getNoteByTitle, addNote, listAll, removeNote };
