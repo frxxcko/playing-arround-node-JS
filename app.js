048d78bff8ba4f0b5a4ba9b696ed0eb20f8bd13e
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-24 00:20:08 -0300

    log the title and body values

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const validator = require('validator')
 const chalk = require('chalk')
-const yargs = require('yargs')
+const yargs = require('yargs');
+const { argv } = require('yargs');
 
 const log = console.log;
 
 
 yargs.command({
-    command: 'list',
-    describe: 'Lists all notes',
-    handler:  () => log('Listing all notes...')
+    command: 'add',
+    describe: 'Add a note',
+    handler: function() {
+        log(`Title: ${chalk.green.inverse(argv.title)}\nBody:  ${chalk.green.inverse(argv.body)}`)
+
+    },
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string"
+        }
+    }
 })
 
 yargs.command({
     command: 'read',
     describe: 'Reads all notes',
-    handler:  () => log('Reading all notes...')
+    handler: function () {
+        log('Reading all notes...')
+    }
 })
 
-log(yargs.argv)
+
+yargs.parse()
