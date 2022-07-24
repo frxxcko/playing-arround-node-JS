9a41b55a012b00b0118476f1a930352aa2535f4b
Juan Basualdo <basualdo.ij@gmail.com>
2022-07-24 15:25:19 -0300

    Remove check point test files

--- playground/check-point.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const data = require('./check-point.json')
-
-const dataStringified = JSON.stringify({ ...data, name: "Malena", age: 28})
-
-fs.writeFileSync('check-point.json', dataStringified)
